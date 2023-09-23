8f9504e9dddf8c43780b6e474f94410d445d15ae
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-09-23 22:51:10 +0200

    finished all the functions

--- contacts.js
+++ contacts.js
@@ -1,30 +1,64 @@
 const fs = require("fs/promises");
 const path = require("path");
+const { nanoid } = require("nanoid");
 
 const contactsPath = path.resolve(__dirname, "db", "contacts.json");
 
+const updateContacts = (allContacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
 };
 
-const getContactById = (contactId) => {
-  // ...твой код. Возвращает объект контакта с таким id. Возвращает null, если объект с таким id не найден.
+const getContactById = async (contactId) => {
+  const allContacts = await listContacts();
+
+  const contact = allContacts.find((item) => item.id === contactId);
+  return contact || null;
 };
 
-const removeContact = (contactId) => {
-  // ...твой код. Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
+const addContact = async ({ name, email, phone }) => {
+  const allContacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+
+  const existingContact = allContacts.find(
+    (contact) => contact.name === name && contact.email === email
+  );
+
+  if (existingContact) {
+    console.log("Contact already exists.");
+    return null;
+  }
+
+  allContacts.push(newContact);
+  await updateContacts(allContacts);
+  return newContact;
 };
 
-const addContact = (name, email, phone) => {
-  // ...твой код. Возвращает объект добавленного контакта.
+const removeContact = async (contactId) => {
+  const allContacts = await listContacts();
+
+  const indexToRemove = allContacts.findIndex((item) => item.id === contactId);
+
+  if (indexToRemove === -1) {
+    return null;
+  }
+  const removedContact = allContacts.splice(indexToRemove, 1)[0];
+  await updateContacts(allContacts);
+  return removedContact;
 };
 
 const contactsOptions = {
   listContacts,
-  //   getContactById,
-  //   removeContact,
-  //   addContact,
+  addContact,
+  getContactById,
+  removeContact,
 };
 
 module.exports = contactsOptions;
--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const contactsOptions = require("./contacts");
 
-const contactsAction = async ({ action, id }) => {
+const contactsAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const contactsList = await contactsOptions.listContacts();
-      return console.log(contactsList);
+      return console.log("allContacts", contactsList);
+    case "getById":
+      const oneContact = await contactsOptions.getContactById(id);
+      return console.log("oneContact:", oneContact);
+    case "addContact":
+      const newItem = await contactsOptions.addContact({ name, email, phone });
+      return console.log("newContact:", newItem);
+    case "deleteContact":
+      const contactToDelete = await contactsOptions.removeContact(id);
+      return console.log("deletedContact:", contactToDelete);
 
     default:
+      console.log("Unknown action");
       break;
   }
 };
 
-contactsAction({ action: "list" });
+// contactsAction({ action: "list" });
+
+// contactsAction({ action: "getById", id: "AeHIrLTr6JkxGE6SN-0Rw" });
+
+// contactsAction({
+//   action: "addContact",
+//   name: "Jack",
+//   email: "jack@mail.com",
+//   phone: "5555555",
+// });
+
+contactsAction({ action: "deleteContact", id: "AeHIrLTr6JkxGE6SN-0Rw" });
